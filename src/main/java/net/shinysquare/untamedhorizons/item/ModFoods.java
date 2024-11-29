6ac1805adec44680627caa3ff02441e6e63d5ae3
ShinySquare <luke.yan2019@gmail.com>
2024-11-29 13:47:03 -0800

    New Stuff

--- src/main/java/net/shinysquare/untamedhorizons/item/ModFoods.java
+++ src/main/java/net/shinysquare/untamedhorizons/item/ModFoods.java
@@ -7,5 +7,4 @@ import net.minecraft.world.food.FoodProperties;
 public class ModFoods {
     public static final FoodProperties STRAWBERRY = new FoodProperties.Builder().nutrition(2).fast()
             .saturationMod(0.2f).effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 200), 0.1f).build();
-
 }
